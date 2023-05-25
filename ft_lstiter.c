bac591d0d78099a22d41ad8e9c804ca1a8cb4237
ibeliaie <ibeliaie@student.42wolfsburg.de>
2023-05-25 15:15:40 +0200

    Change while condition

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: ibeliaie <ibeliaie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 12:15:36 by ibeliaie          #+#    #+#             */
-/*   Updated: 2023/05/20 12:17:25 by ibeliaie         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:36:47 by ibeliaie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst || !f)
 		return ;
-	while (lst != NULL)
+	while (lst)
 	{
 		f(lst->content);
 		lst = lst->next;
